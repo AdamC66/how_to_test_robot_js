1eba911dff5f83c10e59f94533bad149f3ae85ca
adamc66 <adam.cote66@gmail.com>
2019-08-26 14:14:23 -0400

    completed robot testing assignment"

--- robot.test.js
+++ robot.test.js
@@ -1,66 +1,73 @@
 const {newRobot, station, isWorkday, prioritizeTasks}  = require("./robot.js");
 
 // remove .skip when you're ready to implement the test
-test.skip('test_that_foreign_robot_needing_repairs_sent_to_station_1', () => {
+test('test_that_foreign_robot_needing_repairs_sent_to_station_1', () => {
   // arrange
-
+  robot = newRobot(true, true, false)
   // act
-
+  stationReturn = station(robot)
   // assert
+  expect(stationReturn).toEqual(1)
 });
 
-test.skip('test_that_vintage_robot_needing_repairs_sent_to_station_2', () => {
+test('test_that_vintage_robot_needing_repairs_sent_to_station_2', () => {
   // arrange
-
+  robot = newRobot(true, false, true)
   // act
-
+  stationReturn = station(robot)
   // assert
+  expect(stationReturn).toEqual(2)
 });
 
-test.skip('test_that_standard_robot_needing_repairs_sent_to_station_3', () => {
-  // arrange
-
+test('test_that_standard_robot_needing_repairs_sent_to_station_3', () => {
+  robot = newRobot(true, false, false)
   // act
-
+  stationReturn = station(robot)
   // assert
+  expect(stationReturn).toEqual(3)
 });
 
-test.skip('test_that_robot_in_good_condition_sent_to_station_4', () => {
-  // arrange
-
+test('test_that_robot_in_good_condition_sent_to_station_4', () => {
+  robot = newRobot(false, false, false)
   // act
-
+  stationReturn = station(robot)
   // assert
+  expect(stationReturn).toEqual(4)
 });
 
-test.skip('test_prioritize_tasks_with_empty_todo_list_returns_negative_one', () => {
-  // arrange
-
+test('test_prioritize_tasks_with_empty_todo_list_returns_negative_one', () => {
+  robot = newRobot(false, false, false)
+  robot.todos=[]
   // act
-
+  stationReturn = prioritizeTasks(robot)
   // assert
+  expect(stationReturn).toEqual(-1)
 })
 
-test.skip('test_prioritize_tasks_with_todos_returns_max_todo_value', () => {
-  // arrange
-
+test('test_prioritize_tasks_with_todos_returns_max_todo_value', () => {
+  robot = newRobot(false, false, false)
+  robot.todos.push(4);
+  robot.todos.push(2);
   // act
-
+  stationReturn = prioritizeTasks(robot)
   // assert
+  expect(stationReturn).toEqual(4)
 });
 
-test.skip('test_workday_on_day_off_returns_false', () => {
-  // arrange
-
+test('test_workday_on_day_off_returns_false', () => {
+  robot = newRobot(false, false, false)
+  robot.dayOff = 'Tuesday'
   // act
-
+  stationReturn = isWorkday(robot, 'Tuesday')
   // assert
+  expect(stationReturn).toEqual(false)
 });
 
-test.skip('test_workday_not_day_off_returns_true', () => {
-  // arrange
-
+test('test_workday_not_day_off_returns_true', () => {
+  robot = newRobot(false, false, false)
+  robot.dayOff = 'Tuesday'
   // act
-
+  stationReturn = isWorkday(robot, 'Wednesday')
   // assert
+  expect(stationReturn).toEqual(true)
 });
